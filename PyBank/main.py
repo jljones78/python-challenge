d1d453aae54b2d2baf25cc8aeae40ecf523b1fd2
jljones78 <jljones78@gmail.com>
2019-11-19 17:12:46 -0600

    working sum

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,30 +1,33 @@
+import os
 import csv
 
 # filepath = os.path.join('~','Users','JLJones','Desktop','bootcamp','UT-MCB-DATA-PT-11-2019-U-C','Homework','02-Python','Instructions','pybank','resources','budgetdata.csv')
 
-
-
-# with open('budget_data.csv','r') as budget_data:
-#     budget_csv = csv.reader(budget_data)
-#     header = next(budget_data)
-#     for row in budget_csv:
-#         income_row = row[1]
 #         print(sum(map(int, income_row)))
     
+
 # with open('budget_data.csv','r') as budget_data:
 #     budget_csv = csv.reader(budget_data)
 #     header = next(budget_data)
-#     for row in budget_csv:
+#     for line in budget_data:
+#         row = line.strip().split(",")
+#         date_row = row[0]
 #         income_row = row[1]
-#         if income_row[:1] == '-':
-#             neg_income = income_row.replace('-','')
-#             print(sum(map(int, neg_income)))
 
+
+all_income = []
 with open('budget_data.csv','r') as budget_data:
-    budget_csv = csv.reader(budget_data)
-    header = next(budget_data)
+    budget_csv = csv.reader(budget_data, delimiter=',')
+    csv_header = next(budget_csv)
     for row in budget_csv:
-        income_row = row[1]
-        if income_row[:1] == '-':
-            neg_income = income_row.replace('-','')
+        all_income.append(int(row[1]))
+    print(sum(all_income))
+
+
+
+
+
+        # if income_row[:1] == '-':
+        #     neg_income = income_row.replace('-','')
+
 
